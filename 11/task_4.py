57c25d4e5cf0ddd226c05252e7aed6f1f16951c1
Kovalenko Konstantin <94693119+KovalenkoKonstantin@users.noreply.github.com>
2024-03-04 12:37:58 +0300

    Create task_4.py

---
+++ 11/task_4.py
@@ -0,0 +1,8 @@
+# Дано положительное действительное число X.
+# Выведите его первую цифру после десятичной точки.
+# При решении этой задачи нельзя пользоваться условной инструкцией, циклом или строками
+import math
+
+input_number = float(input('Enter a positive float number: '))
+
+print(math.floor((input_number - math.floor(input_number)) * 10))
