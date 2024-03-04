6df76f76f1b59ac9e47008ea3c0811cdfb8c8a69
Kovalenko Konstantin <94693119+KovalenkoKonstantin@users.noreply.github.com>
2024-03-04 09:02:09 +0300

    Create task_7.py

---
+++ 10/task_7.py
@@ -0,0 +1,33 @@
+# Напишите программу,
+# которая получает на вход количество уровней пирамиды и выводит их на экран,
+
+# Пример:
+#
+#             1
+#          3     5
+#       7     9     11
+#    13    15    17    19
+# 21    23    25    27    29
+number_of_levels = int(input("Enter the number of pyramid levels: "))
+
+number = 1
+for i in range(1, number_of_levels + 1):
+    row = ' ' * 3 * (number_of_levels - i)
+    for j in range(i):
+
+        if number == 1:
+            adjust = ''
+        elif 1 < number < 10:
+            adjust = ' ' * 5
+        elif 10 < number < 100:
+            adjust = ' ' * 4
+        elif 100 < number < 1000:
+            adjust = ' ' * 3
+        elif 1000 < number < 10000:
+            adjust = ' ' * 2
+        else:
+            adjust = ' '
+
+        row += str(number) + adjust
+        number += 2
+    print(row)
