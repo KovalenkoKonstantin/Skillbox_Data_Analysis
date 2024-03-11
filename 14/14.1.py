75bfe1ab8881b5fd37c7b768fc200d61a507eaa3
Kovalenko Konstantin <94693119+KovalenkoKonstantin@users.noreply.github.com>
2024-03-11 08:37:24 +0300

    Update

---
+++ 14/14.1.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+print(np.sqrt(17))
+print(np.log(4))
+print(np.mean([20, 40, 30, 450, 45, 30]))
+print(np.median([20, 40, 30, 450, 45, 30]))
+print(np.array([2, 3, 4]))
+print(np.array([[5, 2, 3.5], [1, 9, 6.5]]))
+print(np.array([[[6, 3],
+                 [6, 8]],
+                [[1, 0],
+                [0, 1]]]))
