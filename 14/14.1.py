3fa837fda3fd613195d07edcc357b4d17b2fb0a9
Kovalenko Konstantin <94693119+KovalenkoKonstantin@users.noreply.github.com>
2024-03-12 08:46:02 +0300

    Update 14.1.py

--- 14/14.1.py
+++ 14/14.1.py
@@ -9,6 +9,15 @@ print(np.array([[5, 2, 3.5], [1, 9, 6.5]]))
 print(np.array([[[6, 3],
                  [6, 8]],
                 [[1, 0],
-                [0, 1]]]))
+                 [0, 1]]]))
 print(np.array([['Ann', ' Kate'],
-       [23, 24]]))
+                [23, 24]]))
+turnout = np.array([0.62, 0.43, 0.79, 0.56])
+print(turnout)
+print(turnout * 100)
+print(np.array([2, 3, 8, ]) + np.array([7, 8, 9]))
+week1 = np.array([4000, 0, 2000, 0, 1200])
+week2 = np.array([1000, 2000, 0, 0, 3500])
+week_all = week1 + week2
+print(week_all)
+(week1 + week2) / 2
