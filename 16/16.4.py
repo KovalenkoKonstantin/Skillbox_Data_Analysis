da8753ac33209eebe6fb7db835af48fe33d706f6
Kovalenko Konstantin <94693119+KovalenkoKonstantin@users.noreply.github.com>
2024-03-21 07:53:05 +0300

    Create 16.4.py

---
+++ 16/16.4.py
@@ -0,0 +1,25 @@
+import pandas as pd
+df = pd.DataFrame([['Anna', 23, 3],
+             ['Sam', 36, 12],
+             ['Bill', 33, 10],
+             ['Moica', 25, 7],
+             ['Lisa', 27, 7],
+             ['Peter', 32, None]])
+df.columns = ['name', 'age', 'expr']
+
+print(df.info())
+print(df.shape)
+print(df.shape[0])
+print(df.shape[1])
+print(df.describe())
+print(df.columns)
+
+print(df.columns)
+aaa = list(df.columns)
+aaa[2] = 'experience'
+print(list(df.columns))
+print(aaa)
+
+# df.columns[2] = 'experience'
+
+print(df.index)
