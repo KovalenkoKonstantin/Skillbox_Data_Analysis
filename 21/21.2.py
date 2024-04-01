21e1044658ba009e1a2b33e63122076d3e337a8c
Kovalenko Konstantin <94693119+KovalenkoKonstantin@users.noreply.github.com>
2024-04-01 11:16:50 +0300

    Update 21.2.py

--- 21/21.2.py
+++ 21/21.2.py
@@ -1,13 +1,25 @@
 import sqlalchemy
+import pandas as pd
 import psycopg2
+from sqlalchemy.ext.asyncio import create_async_engine
 
 # conn = 'postgresql+psycopg2://readonly:6hajV34RTQfmxhS@dsstudents.skillbox.ru:5432/db_ds_students'
 # conn = 'jdbc:sqlserver://msk-sql-02;database=RKM'
-conn = 'mssql+pyodbc://rkm:UF5rrXp49IgA1$f6@msk-sql-02/RKM?trusted_connection=yes&driver=ODBC+Driver+18+for+SQL+Server'
+# conn = 'mssql:pymssql://rkm:UF5rrXp49IgA1$f6@msk-sql-02/RKM'
+conn = 'mssql+pymssql://rkm:UF5rrXp49IgA1$f6@msk-sql-02/RKM?charset=utf8'
 
 engine = sqlalchemy.create_engine(conn)
+# engine = create_async_engine(
+#     "mssql+aioodbc://rkm:UF5rrXp49IgA1$f6@msk-sql-02/RKM?"
+#     "driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes"
+# )
 connect = engine.connect()
 
 inspector = sqlalchemy.inspect(engine)
 print(inspector.get_table_names())
-print(inspector)
+# print(inspector)
+
+df = pd.read_sql("select * from AccrualType", connect)
+print(df.head())
+
+print(df.iloc[1:, :])
