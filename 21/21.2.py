b8168ffcbccba48a8afcfc2814bd675c66f8c830
Kovalenko Konstantin <94693119+KovalenkoKonstantin@users.noreply.github.com>
2024-04-01 10:08:31 +0300

    Update 21.2.py

--- 21/21.2.py
+++ 21/21.2.py
@@ -1,7 +1,9 @@
 import sqlalchemy
 import psycopg2
 
-conn = 'postgresql+psycopg2://readonly:6hajV34RTQfmxhS@dsstudents.skillbox.ru:5432/db_ds_students'
+# conn = 'postgresql+psycopg2://readonly:6hajV34RTQfmxhS@dsstudents.skillbox.ru:5432/db_ds_students'
+# conn = 'jdbc:sqlserver://msk-sql-02;database=RKM'
+conn = 'mssql+pyodbc://rkm:UF5rrXp49IgA1$f6@msk-sql-02/RKM?trusted_connection=yes&driver=ODBC+Driver+18+for+SQL+Server'
 
 engine = sqlalchemy.create_engine(conn)
 connect = engine.connect()
