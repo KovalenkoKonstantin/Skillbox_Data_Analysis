89ce0b3a6740d66105c7e6d9b49ab1abd98f9289
Kovalenko Konstantin <94693119+KovalenkoKonstantin@users.noreply.github.com>
2024-05-02 08:34:03 +0300

    Create 123.py

---
+++ pandas_lessons/123.py
@@ -0,0 +1,8 @@
+﻿import pandas as pd
+from xlwings import load
+from xlwings import view
+
+df = pd.DataFrame(data={'one': [0, 1, 2, 3, 4, 5],
+                        'two': [6, 7, 8, 9, 10, 11]})
+
+view(df)
